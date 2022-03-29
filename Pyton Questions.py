8dfdf4ab8baecc3bfc1951c2db7004ce8d0f09e3
YoungKimosabi <100723041+YoungKimosabi@users.noreply.github.com>
2022-03-29 14:32:33 -0500

    Add files via upload

---
+++ Pyton Questions.py
@@ -0,0 +1,55 @@
+#Question 1.)
+
+#def hello_name(user_name):
+#   return("Hello " + user_name)
+#print(hello_name("Kyle"))
+
+#Question 2.)
+
+#def Odd_number():
+#    first_odd = 0
+#    while first_odd < 100:
+#        first_odd += 1
+#        if first_odd % 2 == 0:
+#           continue
+#        print(first_odd)
+    
+#Odd_number()
+
+#Question 3.)
+
+#def max_num_in_list(a_list):
+#    print(max(a_list))
+
+#max_num_in_list([1, 346, 460, 3, 6])
+
+
+#Question 4.)
+
+#def is_leap_year(a_year):
+#    if a_year % 4 == 0:
+#        print("This year is a leap year!")
+#    elif a_year % 100 and 400 == 0:
+#        print("This year is a leap year!")
+#    else:
+#        print("This year is NOT a leap year!")
+
+#is_leap_year(2420)
+
+#Question 5.)
+
+#def is_consecutive(a_list):
+#    for i in range(len(a_list)-1):
+#        if a_list[i] +1 != a_list[i+1]:
+#            print("false")
+#        else:
+#            print("true")
+
+#is_consecutive([4, 5, 6, 7, 8])
+      
+
+
+    
+    
+
+
